aaf8961213d0b74151b80d7f7fdb5f4bbb272c25
Akash kushwah <63041028+professor-4@users.noreply.github.com>
2021-01-22 14:33:20 +0530

    final

--- 100-day-of-code/19 Day Turtle/main.py
+++ 100-day-of-code/19 Day Turtle/main.py
@@ -1,34 +1,43 @@
 from turtle import Turtle, Screen
+import random
 
-tim = Turtle()
+is_race_on = False
 screen = Screen()
+screen.setup(width=500, height=400)
+screen.bgcolor("black")
 
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "white", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
-def move_forwards():
-    tim.forward(10)
+# Create 6 turtles
 
-def move_backwards():
-    tim.backward(10)
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
 
-def turn_left():
-    new_heading = tim.heading() + 10
-    tim.setheading(new_heading)
+if user_bet:
+    is_race_on = True
 
-def turn_right():
-    new_heading = tim.heading() - 10
-    tim.setheading(new_heading)
+while is_race_on:
+    for turtle in all_turtles:
+        # 230 is 250 - half the width of the turtle.
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
 
-def clear():
-    tim.clear()
-    tim.penup()
-    tim.home()
-    tim.pendown()
+        # Make each turtle move a random amount.
 
-screen.listen()
-screen.onkey(move_forwards, "Up")
-screen.onkey(move_backwards, "Down")
-screen.onkey(turn_left, "Left")
-screen.onkey(turn_right, "Right")
-screen.onkey(clear, "c")
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
 screen.exitonclick()
+
